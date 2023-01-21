7f21827376d17888e39a68708a4a927be0796ebc
a.kebriaei <a.kebriaei@KEBRIYAEE-DEV>
2023-01-21 16:09:08 +0330

    .

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use \App\Http\Livewire\HomeComponent;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
 
 //Route::get('/', function () {
 //    return view('welcome');
